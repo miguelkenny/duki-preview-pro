cf24c9b99eecfa9072bcc1b0550ed15a566e4567
miguelkenny <miguelcastrokenny@gmail.com>
2025-06-13 22:37:01 -0300

    Versión inicial del proyecto Duki Preview Pro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Nueva canción de Duki 🔥</h1>
 <p>Haz clic para escuchar el preview antes que nadie.</p>
 
-<a href="/access">
+<a href="/access-preview">
   <button style="font-size: 1.2rem; padding: 10px 20px;"
     >Escuchar preview</button
   >
